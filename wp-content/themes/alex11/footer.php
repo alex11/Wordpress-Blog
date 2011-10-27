fc6026a12fc55b9f6fc3ff035321cbb7f40d96a6
bmen <bmenningen@beatandmusic.com>
2011-10-27 15:31:46 +0200

    new footer style for 4 column widgets

--- wp-content/themes/alex11/footer.php
+++ wp-content/themes/alex11/footer.php
@@ -4,7 +4,7 @@
 		<?php do_action( 'bp_before_footer' ) ?>
 
 		<div id="footer">
-			<div id="footer-image">&nbsp;</div>
+			<div id="footer-image"><img src="/wp-content/uploads/2011/10/footer-image-asamblea.png" alt="asamblea group"/></div>
 			<?php if ( is_active_sidebar( 'first-footer-widget-area' ) || is_active_sidebar( 'second-footer-widget-area' ) || is_active_sidebar( 'third-footer-widget-area' ) || is_active_sidebar( 'fourth-footer-widget-area' ) ) : ?>
 				<div id="footer-widgets">
 					<?php get_sidebar( 'footer' ) ?>
@@ -23,17 +23,18 @@
 
 		<?php wp_footer(); ?>
 	</div>
-	<div id="footer-color">&nbsp;</div>
+
 	<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.alex11.org/stats/" : "http://www.alex11.org/stats/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://www.alex11.org/stats/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
+		var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.alex11.org/stats/" : "http://www.alex11.org/stats/");
+		document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+			piwikTracker.trackPageView();
+			piwikTracker.enableLinkTracking();
+			} catch( err ) {}
+	</script><noscript><p><img src="http://www.alex11.org/stats/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
 	</body>
 
 </html>
